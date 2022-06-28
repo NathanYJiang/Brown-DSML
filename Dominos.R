3a23be1a88f3c59956eca272bff5be9bc993740e
Nathan Jiang <108350233+NathanYJiang@users.noreply.github.com>
2022-06-28 16:50:24 -0400

    Add files via upload

---
+++ Dominos.R
@@ -0,0 +1,7 @@
+library(readxl)
+Dominos <- read_excel("~/Downloads/Dominos.xlsx")
+View(Dominos)
+
+#Perform the one-tailed t test
+t.test(Dominos$Time, alternative = "less", mu=173.62)
+
---
+++ Titanic.R
@@ -0,0 +1,29 @@
+library(readr)
+Titanic <- read_csv("Downloads/Titanic.csv")
+
+Titanic$Pclass = factor(Titanic$Pclass)
+Titanic$Survived = factor(Titanic$Survived)
+#Titanic$Sex = factor(Titanic$Sex)
+
+#Descriptive Statistics
+summary(Titanic)
+str(Titanic)
+
+# Create bar plot using ggplot2
+ggplot(Titanic, aes(x=Pclass)) + geom_bar()
+ggplot(Titanic, aes(x=Pclass, color = Sex, fill = Sex)) + 
+  geom_bar() + theme_bw() + theme(legend.position="top")
+ggplot(Titanic, aes(x=Pclass, color = Sex, fill = Sex)) + 
+  geom_bar(position="dodge") + theme_bw() + theme(legend.position="top")
+
+# Create box plot using ggplot2
+ggplot(Titanic, aes(Sex, Age)) + geom_boxplot()
+ggplot(Titanic, aes(x=Pclass, y=Age, fill = Sex)) + 
+  geom_boxplot(outlier.color = "dark orange", outlier.shape = 4, notch = TRUE) + 
+  coord_flip() + theme_bw() + theme(legend.position="top")
+
+#Create histogram using ggplot2
+ggplot(Titanic, aes(x=Age)) + geom_histogram(binwidth = 10)
+ggplot(Titanic, aes(x=Age, color=Sex, fill = Sex)) + 
+  geom_histogram(binwidth = 10) + theme_bw() + theme(legend.position="top")
+
