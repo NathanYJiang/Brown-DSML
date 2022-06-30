b1e2cb9d3b4d95a24d66a67321902b33dda18d35
Nathan Jiang <108350233+NathanYJiang@users.noreply.github.com>
2022-06-30 17:24:58 -0400

    Add files via upload

--- Dominos.R
+++ Dominos.R
@@ -5,4 +5,31 @@ View(Dominos)
 #Perform the one-tailed t test
 t.test(Dominos$Time, alternative = "less", mu=173.62)
 
+# One Sample t-test
+# 
+# data:  Dominos$Time
+# t = -4.2568, df = 29, p-value = 9.921e-05
+# alternative hypothesis: true mean is less than 173.62
+# 95 percent confidence interval:
+#   -Inf 164.7089
+# sample estimates:
+#   mean of x 
+# 158.789 
 
+# Since the p-value (9.921e-05) < critical value (0.05), we accept Ha 
+
+#Perform the two-tailed t test
+t.test(Dominos$Time, alternative='two.sided', mu=173.62)
+
+# One Sample t-test
+# 
+# data:  Dominos$Time
+# t = -4.2568, df = 29, p-value = 0.0001984
+# alternative hypothesis: true mean is not equal to 173.62
+# 95 percent confidence interval:
+#   151.6632 165.9148
+# sample estimates:
+#   mean of x 
+# 158.789 
+
+# Since the p-value (0.0001984) < critical value (0.05), we accept Ha 
